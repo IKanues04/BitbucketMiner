2f67e266de514524d60ddd1f4f4d9a8385f9e4c8
FIRST_NAME LAST_NAME <dmenae@est.ups.edu.ec>
2025-04-23 11:49:50 +0200

    Reinicio de clases (Tercer avance)

---
+++ src/main/java/aiss/bitbucketminer/model/Comment.java
@@ -0,0 +1,68 @@
+package aiss.bitbucketminer.model;
+
+public class Comment {
+
+    // ATRIBUTOS
+
+    private String id;
+    private String body;
+    private String createdAt;
+    private String updatedAt;
+
+    private User author;
+
+    // INICIALIZACION DE ATRIBUTOS
+
+    public Comment () {}
+
+    public Comment(String id, String body, String createdAt, String updatedAt, User author) {
+        this.id = id;
+        this.body = body;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.author = author;
+    }
+
+    // GETTERS Y SETTERS
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
---
+++ src/main/java/aiss/bitbucketminer/model/Issue.java
@@ -0,0 +1,138 @@
+package aiss.bitbucketminer.model;
+
+import java.util.List;
+
+public class Issue {
+
+    private String id;
+    private String title;
+    private String description;
+    private String state;
+    private String createdAt;
+    private String updatedAt;
+    private String closedAt;
+    private List<String> labels;
+    private int votes;
+
+    private User assignee;
+    private User author;
+    private List<Comment> comments;
+
+    // INICIALIZACION DE ATRIBUTOS
+
+    public Issue () {}
+
+    public Issue(String id, String title, String description, String state, String createdAt, String updatedAt, String closedAt, List<String> labels, int votes, User assignee, User author, List<Comment> comments) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.state = state;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.closedAt = closedAt;
+        this.labels = labels;
+        this.votes = votes;
+        this.assignee = assignee;
+        this.author = author;
+        this.comments = comments;
+    }
+
+    // SETTERS Y GETTERS
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getClosedAt() {
+        return closedAt;
+    }
+
+    public void setClosedAt(String closedAt) {
+        this.closedAt = closedAt;
+    }
+
+    public List<String> getLabels() {
+        return labels;
+    }
+
+    public void setLabels(List<String> labels) {
+        this.labels = labels;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void setVotes(int votes) {
+        this.votes = votes;
+    }
+
+    public User getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(User assignee) {
+        this.assignee = assignee;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+}
