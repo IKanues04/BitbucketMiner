08c7d0d8ed141b39e945e7ac0422cef78ac4e118
FIRST_NAME LAST_NAME <dmenae@est.ups.edu.ec>
2025-04-23 11:49:38 +0200

    Reinicio de clases (Tercer avance)

---
+++ src/main/java/aiss/bitbucketminer/model/Project.java
@@ -0,0 +1,74 @@
+package aiss.bitbucketminer.model;
+
+
+import java.util.List;
+
+public class Project {
+
+    // ATRIBUTOS
+    private String id;
+    private String name;
+    private String webUrl;
+
+    private List<Issue> issues;
+    private List<Commit> commits;
+
+    // INICIALIZAR ATRIBUTOS
+
+    public Project () {
+
+    }
+
+    public Project (String id, String name, String webUrl, List<Issue> issues, List<Commit> commits) {
+        this.id = id;
+        this.name = name;
+        this.webUrl = webUrl;
+
+        this.issues = issues;
+        this.commits = commits;
+    }
+
+    // GETTERS Y SETTERS
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+
+    public List<Issue> getIssues() {
+        return issues;
+    }
+
+    public void setIssues(List<Issue> issues) {
+        this.issues = issues;
+    }
+
+    public List<Commit> getCommits() {
+        return commits;
+    }
+
+    public void setCommits(List<Commit> commits) {
+        this.commits = commits;
+    }
+
+
+}
---
+++ src/main/java/aiss/bitbucketminer/model/User.java
@@ -0,0 +1,67 @@
+
+package aiss.bitbucketminer.model;
+
+public class User {
+
+    // ATRIBUTOS
+
+    private String id;
+    private String username;
+    private String name;
+    private String avatarUrl;
+    private String webUrl;
+
+    // INICIALIZACION DE ATRIBUTOS
+
+    public User () {}
+
+    public User(String id, String username, String name, String avatarUrl, String webUrl) {
+        this.id = id;
+        this.username = username;
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+        this.webUrl = webUrl;
+    }
+
+    // GETTERS Y SETTERS
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public void setAvatarUrl(String avatarUrl) {
+        this.avatarUrl = avatarUrl;
+    }
+
+    public String getWebUrl() {
+        return webUrl;
+    }
+
+    public void setWebUrl(String webUrl) {
+        this.webUrl = webUrl;
+    }
+}
